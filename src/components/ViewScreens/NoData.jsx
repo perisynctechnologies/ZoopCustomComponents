b0bd8b5f49d5c68343e4eb0812987399c1637c27
Yasser Arafat M <97438664+arafatperisync@users.noreply.github.com>
2024-12-03 14:45:46 +0530

    import issue fixes

--- src/components/ViewScreens/NoData.jsx
+++ src/components/ViewScreens/NoData.jsx
@@ -3,7 +3,7 @@ import Box from "@mui/material/Box";
 import { Button, SvgIcon, Typography } from "@mui/material";
 import PlusIcon from "@untitled-ui/icons-react/build/esm/Plus";
 // import { NoDataSvg } from "../../assets/ImageConstant";
-import { NoDataSvg } from "../../assets/images/errors/no-data.svg";
+import NoDataSvg from "../../assets/images/errors/no-data.svg";
 
 export const NoData = ({ title, description, bool, button, action }) => {
   return (
