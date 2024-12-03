7e278efb631c1129a412b9e5b3f59bb7ad891918
Yasser Arafat M <97438664+arafatperisync@users.noreply.github.com>
2024-12-03 14:36:33 +0530

    import issues

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Import LoadingScreen from its source
 import { LoadingScreen as LoadingScreenComponent } from "./src/components/ViewScreens/LoadingScreen";
-import { NoData as NoDataComponent } from "./src/components/ViewScreens/LoadingScreen";
+import { NoData as NoDataComponent } from "./src/components/ViewScreens/NoData";
 
 // Export the imported LoadingScreen
 export const LoadingScreen = LoadingScreenComponent;
